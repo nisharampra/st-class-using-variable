5e83b7e779065c8e93e89b9a2758610721f886e3
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-09 00:21:12 +0800

    Add files via upload

---
+++ st classs using variable/st classs using variable/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  st classs using variable
+//
+//  Created by Nisha Ramprasath on 8/11/23.
+//
+
+#include<iostream>
+#include<vector>
+using namespace std;
+    
+int main()
+{
+    vector<int> v={2,4,6,8,10};
+    v.push_back(20);
+    v.push_back(30);
+    vector<int>::iterator itr;
+    cout<<"Using Iterator"<<endl;
+    for(itr=v.begin();itr!=v.end();itr++)
+        cout<<++*itr<<endl;
+    cout<<"Using For Each Loop"<<endl;
+    
+    for(int x:v)
+        cout<<x<<endl;
+}
